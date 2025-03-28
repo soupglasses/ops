d818fc42e36ca52601984d6ccf01ec2429d409e0
Sofie <sofie+git@mailbox.org>
2025-03-28 22:31:27 +0100

    feat: init flux-local at 7.3.0

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     supportedSystems = [
       "aarch64-linux"
       "x86_64-linux"
@@ -13,10 +16,62 @@
           pkgs = nixpkgs.legacyPackages.${system};
         });
   in {
+    packages = eachSystem ({pkgs, ...}: {
+      flux-local = pkgs.python3Packages.buildPythonPackage rec {
+        pname = "flux-local";
+        version = "7.3.0";
+        pyproject = true;
+
+        src = pkgs.fetchFromGitHub {
+          owner = "allenporter";
+          repo = pname;
+          rev = version;
+          hash = "sha256-YzxFw49yMum0F6Z57utUSzxiBK7ME1KxLVqvbdZBHBY=";
+        };
+
+        build-system = with pkgs.python3Packages; [
+          setuptools
+          setuptools-scm
+        ];
+
+        dependencies = with pkgs.python3Packages; [
+          aiofiles
+          nest-asyncio
+          gitpython
+          pyyaml
+          mashumaro
+          pytest
+          pytest-asyncio
+        ];
+
+        buildInputs = with pkgs; [
+          kustomize
+          fluxcd
+          helm
+        ];
+
+        postPatch = ''
+          substituteInPlace flux_local/kustomize.py \
+            --replace-fail 'KUSTOMIZE_BIN = "kustomize"' 'KUSTOMIZE_BIN = "${pkgs.kustomize}/bin/kustomize"' \
+            --replace-fail 'FLUX_BIN = "flux"' 'FLUX_BIN = "${pkgs.fluxcd}/bin/flux"'
+          substituteInPlace flux_local/helm.py \
+            --replace-fail 'HELM_BIN = "helm"' 'HELM_BIN = "${pkgs.helm}/bin/helm"'
+          substituteInPlace tests/tool/__init__.py \
+            --replace-fail 'FLUX_LOCAL_BIN = "flux-local"' "FLUX_LOCAL_BIN = \"$out/bin/flux-local\""
+        '';
+
+        # Majority of tests require git repo.
+        doCheck = false;
+      };
+    });
+
     # -- Development Shells --
     # Scoped environments including packages and shell-hooks to aid project development.
 
-    devShells = eachSystem ({pkgs, ...}: {
+    devShells = eachSystem ({
+      pkgs,
+      system,
+    }: {
       default = pkgs.mkShellNoCC {
         shellHook = ''
           ${pkgs.pre-commit}/bin/pre-commit install --install-hooks --overwrite
@@ -32,6 +87,7 @@
           kubectl
           kubeseal
           fluxcd
+          self.packages.${system}.flux-local
           kustomize
         ];
       };
